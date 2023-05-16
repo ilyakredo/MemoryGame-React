3aaa717e23905cde32ec12e7f93fa011b86c9fbd
Ilyakredo <imoiseenko25@ukr.net>
2023-05-16 18:05:29 +0300

    Game template and shuffle function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css'
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import { images } from "./constants";
+import { useState } from "react";
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards function
+  const shuffleCards = () => {
+    const shuffledCards = [...images, ...images]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: uuidv4() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="game-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img
+                src={card.src}
+                className="card-front"
+                alt="card front side"
+              />
+              <img
+                src="/img/cover.png"
+                className="card-back"
+                alt="card back side"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
